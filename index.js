36f9d0595f1fcd845a0567a406ef8428cc618b32
Partho Protim Roy <parthoroy158@gmail.com>
2025-05-27 22:48:35 +0600

    JWT ADDED IN USERS

---
+++ index.js
@@ -0,0 +1,156 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const port = process.env.port || 5000;
+const app = express()
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+
+// mongodb connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qbyxney.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        const userCollection = client.db("bistroDB").collection("users");
+        const menuCollection = client.db("bistroDB").collection("menu");
+        const reviewsCollection = client.db("bistroDB").collection("reviews");
+        const cartsCollection = client.db("bistroDB").collection("carts");
+
+
+        // Create a jwt token
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ token })
+        })
+
+        // verify the jwt token
+        const verifyToken = (req, res, next) => {
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: "Forbidden Access" })
+            }
+            const token = req.headers.authorization
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+                req.decoded = decoded
+                next()
+
+            })
+            console.log("This after the verify :", token)
+
+        }
+
+
+
+
+        // add admin in the from the user
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const updateDoc = {
+                $set: {
+                    role: "admin",
+                },
+            }
+            const result = await userCollection.updateOne(query, updateDoc);
+            res.send(result)
+        })
+
+        // user delete form the database
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            console.log(id)
+            const result = await userCollection.deleteOne(query)
+            res.send(result)
+        })
+
+        // get the users from the database
+        app.get('/users', verifyToken, async (req, res) => {
+            // console.log('Here is the inside token:', req.headers.authorization)
+            const result = await userCollection.find().toArray()
+            res.send(result)
+        })
+        // users related api
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            // if email already exists that not allow to add store 
+            const query = { email: user.email }
+            const existingUser = await userCollection.findOne(query);
+            if (existingUser) {
+                return res.send({ message: 'User already exists', insertedId: null })
+            }
+            const result = await userCollection.insertOne(user)
+            res.send(result)
+        })
+
+        // get the all menu data form the database
+        app.get('/menu', async (req, res) => {
+            const result = await menuCollection.find().toArray()
+            res.send(result)
+        })
+        // get the reviews data from the database
+        app.get('/reviews', async (req, res) => {
+            const result = await reviewsCollection.find().toArray()
+            res.send(result)
+        })
+
+
+        // get the cart data
+        app.get('/carts', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email }
+            const result = await cartsCollection.find(query).toArray()
+            res.send(result)
+        })
+        // post cart in the data base
+        app.post('/carts', async (req, res) => {
+            const cartItems = req.body
+            // inset email if user exists
+
+            const result = await cartsCollection.insertOne(cartItems)
+            res.send(result)
+        })
+
+        // delete from the cart from the database
+        app.delete('/carts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await cartsCollection.deleteOne(query)
+            res.send(result)
+        })
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Bistro Boss Server')
+})
+
+app.listen(port, () => {
+    console.log(`This is the server port, ${port}`)
+})
