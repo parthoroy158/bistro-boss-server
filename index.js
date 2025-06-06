01d82ae05018c0c1c78d625efb83946b459a3828
Partho Protim Roy <parthoroy158@gmail.com>
2025-06-06 10:56:21 +0600

    admin route fixed

--- index.js
+++ index.js
@@ -40,23 +40,35 @@ async function run() {
             res.send({ token })
         })
 
-        // verify the jwt token
+        // verify token 
         const verifyToken = (req, res, next) => {
+            console.log('Inside the verify token:', req.headers.authorization)
             if (!req.headers.authorization) {
-                return res.status(401).send({ message: "Forbidden Access" })
+                return res.status(401).send({ message: "forbidden access" })
             }
-            const token = req.headers.authorization
-            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
-                req.decoded = decoded
+            const token = req.headers.authorization.split(' ')[1]
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                if (err) {
+                    return res.status(401).send({ message: "forbidden access" })
+                }
+                req.decoded = decoded;
                 next()
-
             })
-            console.log("This after the verify :", token)
-
         }
+        // use verify admin after verify token
+       
 
 
-
+        app.get('/users/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email }
+            const user = await userCollection.findOne(query);
+            let admin = false;
+            if (user) {
+                admin = user?.role === "admin"
+            }
+            res.send({ admin })
+        })
 
         // add admin in the from the user
         app.patch('/users/admin/:id', async (req, res) => {
@@ -82,7 +94,7 @@ async function run() {
 
         // get the users from the database
         app.get('/users', verifyToken, async (req, res) => {
-            // console.log('Here is the inside token:', req.headers.authorization)
+            // console.log('Here is the inside token:', req.headers)
             const result = await userCollection.find().toArray()
             res.send(result)
         })
